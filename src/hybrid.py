4fe377ad676289f037f6df6909ac3e7f9299dcd7
Dominik Soós <soos.domi@gmail.com>
2025-11-29 12:58:36 -0500

    frozen model training

--- src/hybrid.py
+++ src/hybrid.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from fd2nn import FD2NN
 from cnn import CNN
@@ -8,16 +9,23 @@ class HybridFD2NN_CNN(nn.Module):
     """
     def __init__(self, img_size=32, classes=10,
                  fd_layers=2, fd_channels=32,
-                 freeze_frontend=False,
+                 freeze_frontend=False,init_noise_scale=0.0,
                  cnn_channels=(16,32), linear_head=False):
         super().__init__()
         # Front end: FD2NN 
         # We don't care about 'classes' here, we want features.
         self.front = FD2NN(in_ch=1, img_size=img_size, n_layers=fd_layers,
                            hidden_channels=fd_channels, classes=fd_channels) 
+        if freeze_frontend:
+            init_noise_scale = 2.0
+        if init_noise_scale > 0:
+            with torch.no_grad():
+                for p in self.front.phase_masks:
+                    p.data.uniform_(-init_noise_scale, init_noise_scale)
         
         # Back end: CNN
         # Input channels to CNN = hidden_channels of FD2NN
+        
         self.back = CNN(in_ch=fd_channels, classes=classes,
                         channels=cnn_channels, linear=linear_head)
 
